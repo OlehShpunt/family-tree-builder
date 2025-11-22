cb628d06c06478dcad659282c93afd5ae6c20a51
OlehShpunt <olehshpunt2@gmail.com>
2025-11-22 15:35:04 -0800

    feat: add data persistence

--- Program.cs
+++ Program.cs
@@ -1,35 +1,43 @@
-using family_tree_builder.Data;
-using Microsoft.EntityFrameworkCore;
+    using family_tree_builder.Data;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text.Json;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-// Database
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite("Data Source=familytree.db"));
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    // Database
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlite("Data Source=familytree.db"));
 
-var app = builder.Build();
+    // Convert PascalCase fields into camelCase and vice versa when sending to/from browser
+    builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseRouting();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-app.UseAuthorization();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.UseAuthorization();
 
-app.MapStaticAssets();
+    app.MapStaticAssets();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
 
 
-app.Run();
+    app.Run();
