92a6fa73bcaa30e18026cb72ac77881b916359d4
Tafadzwa <193275436+TmuD-05@users.noreply.github.com>
2025-11-24 21:19:26 -0800

    feat: add login and registration page
    - fixed multiple issues with the authentication
    - added a page that was missing for login or register functionality
    - created identity tables that were missing in the DB

--- DbContext/ApplicationDbContext.cs
+++ DbContext/ApplicationDbContext.cs
@@ -4,7 +4,6 @@ using family_tree_builder.Models;
 
 namespace family_tree_builder.Data
 {
-    // Change from DbContext → IdentityDbContext
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
